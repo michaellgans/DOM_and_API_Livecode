70b31a0de9c21f3147827da796c1985fb07fa8ec
michaellgans <michaellgans@Victoria>
2024-07-23 16:41:42 -0500

    M - 4:41 PM - Syntax examples added

--- jQuery_examples.js
+++ jQuery_examples.js
@@ -1,13 +1,41 @@
 // Examples of jQuery vs JavaScript for syntax reference
 
-// SELECTING CONTENT
+// ---------- SELECTING CONTENT ----------
 // JavaScript:
 document.getElementById("id");
 // jQuery:
 $("#id");
 
-// CHANGING CONTENT
+// ---------- CHANGING CONTENT ----------
 // JavaScript:
 document.getElementById("id").innerHTML = "Hello World!";
 // jQuery:
 $("#id").html("Hello World!");
+
+// ---------- EVENT LISTENERS ----------
+// JavaScript:
+document.getElementById("id")
+    .addEventListener("click", function() {
+        alert("Element #id was clicked.");
+    });
+// jQuery:
+$("#id").click(function() {
+    alert("Element #id was clicked.");
+});
+
+// ---------- DOCUMENT READY ----------
+// jQuery:
+$(document).ready(function() {
+    $("id").html("Hello World!");
+    // Other function calls can also go inside here
+    // So everything runs only once the document is ready.
+});
+// JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("demo").innerHTML = "Hello World!";
+});
+
+
+const data = JSON.parse(json);
+console.log(data.states.texas.cities);
+// Output: ["dallas", "irving"]
